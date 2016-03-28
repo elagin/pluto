4b482243285c652968e4fbbdab57e56e168eba4b
Pavel Elagin <elagin.pasha@gmail.com>
2016-03-29 02:18:37 +0300

    create.sql

--- sql/create.sql
+++ sql/create.sql
@@ -1,5 +1,8 @@
 CREATE TABLE last_update(id BIGSERIAL PRIMARY KEY, date timestamp, block_id integer not null);
-CREATE TABLE lines(id BIGSERIAL PRIMARY KEY, date timestamp not null, block_id integer not null, shape geometry);
-CREATE TABLE points(id BIGSERIAL PRIMARY KEY, date timestamp not null, block_id integer not null, shape geometry);
+ALTER TABLE last_update ADD CONSTRAINT constraint_last_update_block_id UNIQUE (block_id);
 
+CREATE TABLE lines(id BIGSERIAL PRIMARY KEY, date timestamp not null, block_id integer not null, shape geometry not null);
+CREATE INDEX idx_lines_shape ON lines USING GIST ( shape );
 
+CREATE TABLE points(id BIGSERIAL PRIMARY KEY, date timestamp not null, block_id integer not null, shape geometry not null);
+CREATE INDEX idx_points_shape ON points USING GIST ( shape );
